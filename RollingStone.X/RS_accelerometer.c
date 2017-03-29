7a7deb94a51be232901e136f119dd75ac6e92def
Guilain LANG <guilain.lang@epfl.ch>
2017-03-29 18:20:24 +0200

    Add files via upload

--- RollingStone.X/RS_accelerometer.c
+++ RollingStone.X/RS_accelerometer.c
@@ -4,14 +4,6 @@
  *
  * Created on 29. mars 2017, 16:48
  */
-//#include "xc.h"
-#include <a_d/e_accelerometer.h>
-#include <RS_accelerometer.h>
 
 
-int RS_acc_update(int* acc_T) {
-    e_get_acc(acc_T+ID_X, acc_T+ID_Y, acc_T+ID_Z);
-    acc_T[ID_X]-=2048;
-    acc_T[ID_Y]-=2048;
-    acc_T[ID_Z]-=2048;
-}
+#include "xc.h"
