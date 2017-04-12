a128bdb67c2a15180ebc9b0fd6f29357125a9e21
Guilain LANG <guilain.lang@epfl.ch>
2017-04-12 18:18:06 +0200

    Add files via upload

---
+++ RollingStone.X/library/a_d/a_d.X/build/default/production/_ext/1472/e_accelerometer.o.d
@@ -0,0 +1,5 @@
+build/default/production/_ext/1472/e_accelerometer.o:  \
+ ../e_accelerometer.c  \
+../e_ad_conv.h  \
+../../motor_led/e_epuck_ports.h  \
+ ../e_accelerometer.h 
---
+++ RollingStone.X/library/a_d/a_d.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=a_d.X.a
+CND_ARTIFACT_PATH_default=dist/default/production/a_d.X.a
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=ad.X.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ad.X.tar
